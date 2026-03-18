25f83eddb4102ada7df848f3a7091962d7d5c16c
Ryan Luong <lumintriet@gmail.com>
2026-03-18 08:27:11 +0700

    feat(ui): define RootLayout and its route.

---
+++ src/routes/__root.tsx
@@ -0,0 +1,43 @@
+/// <reference types="vite/client" />
+
+import type { ReactNode } from "react";
+
+import { createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
+
+import styles from "@/globals.css?url";
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        content: "width=device-width, initial-scale=1",
+        name: "viewport",
+      },
+      {
+        title: "Site Speed",
+      },
+    ],
+    links: [
+      { rel: "stylesheet", href: styles },
+      { type: "image/svg+xml", href: "/favicon.svg", rel: "icon" },
+    ],
+  }),
+  shellComponent: RootDocument,
+});
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html suppressHydrationWarning lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
+    </html>
+  );
+}
